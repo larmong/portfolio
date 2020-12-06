89eb4bc4b7f7c742acc1f802293dc4151e39dfdb
LEEAREUM <larmong@naver.com>
2020-12-07 03:10:00 +0900

    2020.12.07

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,25 @@
 // main.js
 
 // 모바일용 메뉴 (햄버거 모양 변경, 메뉴 열림)
-// $(function(){
-// 	$('.m_gnb').on('click', function(){
-// 		$(this).toggleClass('on')
-// 		$('.header .gnb ul').toggleClass('show');
-// 	});
-// })
-// $(document).ready(function(){
-// 	$('#fullpage').fullpage();
+$(function(){
+	$('.icon').on('click', function(){
+		$(this).toggleClass('on')
+		$('.gnb').toggleClass('show');
+	});
+})
 
-// 	$('.gnb ul li a').on('click', function(){
-// 		var index = $(this).parent().index();
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.desc').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'margin-left':'0px'},1000);
+            }
+            
+        }); 
+    });
+});
 
-// 		$.fn.fullpage.moveTo(index + 1);
-// 	})
-// })
