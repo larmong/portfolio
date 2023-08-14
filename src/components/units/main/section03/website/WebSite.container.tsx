aeb802180f1fe81df175d9322c7643623593f742
larmong <larmong@naver.com>
2023-08-15 02:21:12 +0900

    [ADD] main > project > website

--- src/components/units/main/section03/website/WebSite.container.tsx
+++ src/components/units/main/section03/website/WebSite.container.tsx
@@ -29,34 +29,45 @@ export default function TabWebSite() {
       <LeftText>
         <Content triggerOnce={true}>
           <span>
+            웹 사이드 프로젝트는 저의&nbsp;
             <em onMouseEnter={handleMouse(1)} onMouseLeave={handleMouse(1)}>
-              새로운 아이디어를 코드로 구현
+              웹 개발 역량의 단계를 효과적
             </em>
-            하는 과정을 즐기며, 사용자 경험을 개선하고 멋진 인터페이스를 구축
-            하는 일에 큰 흥미를 가지고 있습니다. 끊임없이 발전하는 기술 동향을
-            따라가며 배우는 것을 좋아하며,&nbsp;
+            으로 보여주는 프로젝트입니다. 이 프로젝트에서는 다양한 프론트엔드
+            기술과 백엔드 기능을 사용하여 사용자들이 흥미롭고 즐거운 경험을 얻을
+            수 있는 웹페이지를 구현하고자 했습니다.
+          </span>
+        </Content>
+        <Content triggerOnce={true}>
+          <span>
+            사용자 친화적인 UI/UX 디자인과 모바일 반응형 구현을 통해 웹
+            환경에서의 편의성과 직관성을 최적화하는 방향을 노력하며 개발해
+            나가고 있습니다. 이를 통해 웹 개발의 다양한 측면을 체험하고,&nbsp;
             <em onMouseEnter={handleMouse(2)} onMouseLeave={handleMouse(2)}>
-              코드를 작성할때 가독성과 유지보수
+              프로젝트 진행 중 마주한 다양한 이슈
             </em>
-            를 고려한 코드를 작성하는것에 집중합니다.
+            들을 해결하며 전반적인 웹 개발 과정을 깊이 이해려고 공부하고
+            있습니다.
           </span>
         </Content>
         <Content triggerOnce={true}>
           <span>
-            포트폴리오에는 저의 프론트엔드&nbsp;
+            뿐만아니라, 웹 사이드 프로젝트를 통해 디자인 단계부터 프론트엔드와
+            백엔드까지의 업무를 경험해보면서, 팀원들과 협업하여 업무를 조화롭게
+            진행하는 과정에서의 중요성을 깨달았고 협업을 위한 의사소통과 역할
+            분담의 중요성을 실제로 체감하게되었습니다.
+          </span>
+        </Content>
+        <Content triggerOnce={true}>
+          <span>
+            또한,&nbsp;
             <em onMouseEnter={handleMouse(3)} onMouseLeave={handleMouse(3)}>
-              개발자로서의 성장과정
+              자체적인 QA를 진행
             </em>
-            을 확인하실 수 있도록 다양한 사이드 프로젝트들을 담았습니다.
-            최근에는 문제 해결 과정을 Readme를 통해 확인 할 수 있도록 정리하며
-            진행하고있습니다.
-          </span>
-        </Content>
-        <Content triggerOnce={true}>
-          <span>
-            포트폴리오를 통해 저에 대해 더 자세히 알아가시면 좋을 것 같습니다.
-            <br />
-            감사합니다:) ☺️
+            하면서 지속적인 품질 개선을 추구하는 것에도 큰 관심과 노력을
+            기울이고 있습니다. 이를 통해 웹 개발자로서의 역량을 지속적으로
+            향상시키고, 사용자들에게 더 나은 경험을 제공할 수 있는 방향이 될 수
+            있도록 고민하고 있습니다.
           </span>
         </Content>
       </LeftText>
@@ -66,15 +77,15 @@ export default function TabWebSite() {
           <Image>
             {isHover01 ? (
               <Fade>
-                <img src="/images/intro1.gif" alt="intro" />
+                <img src="/images/website1.gif" alt="website" />
               </Fade>
             ) : isHover02 ? (
               <Fade>
-                <img src="/images/intro2.gif" alt="intro" />
+                <img src="/images/website2.gif" alt="website" />
               </Fade>
             ) : isHover03 ? (
               <Fade>
-                <img src="/images/intro3.gif" alt="intro" />
+                <img src="/images/website3.gif" alt="website" />
               </Fade>
             ) : (
               ""
