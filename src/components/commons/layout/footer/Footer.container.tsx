97fa19642095b9f33e28fa99fa3303384afc7057
larmong <larmong@naver.com>
2023-08-15 03:15:46 +0900

    [FIX] footer icon

--- src/components/commons/layout/footer/Footer.container.tsx
+++ src/components/commons/layout/footer/Footer.container.tsx
@@ -4,18 +4,34 @@ import { BsGithub } from "react-icons/bs";
 import { MdMail } from "react-icons/md";
 
 export default function Footer() {
+  const icon_list = [
+    {
+      title: "github",
+      icon: <BsGithub />,
+      url: "https://github.com/larmong",
+    },
+    {
+      title: "velog",
+      icon: <SiVelog />,
+      url: "https://velog.io/@larmong",
+    },
+    {
+      title: "email",
+      icon: <MdMail />,
+      url: "",
+    },
+  ];
+
+  const onClickMoveToSite = (url: string) => () => {
+    window.open(`${url}`, "_blank");
+  };
+
   return (
     <Wrapper>
       <Pages>
-        <PageIcon>
-          <BsGithub />
-        </PageIcon>
-        <PageIcon>
-          <SiVelog />
-        </PageIcon>
-        <PageIcon>
-          <MdMail />
-        </PageIcon>
+        {icon_list.map((el) => (
+          <PageIcon onClick={onClickMoveToSite(el.url)}>{el.icon}</PageIcon>
+        ))}
       </Pages>
       <Copy>copyright ⓒ 2023 All rights reserved by larmong.</Copy>
     </Wrapper>
