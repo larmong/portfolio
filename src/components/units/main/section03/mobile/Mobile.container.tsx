3b3a08cf2fa38f8eb4d4d30c02e8b92a68b2ce1e
larmong <larmong@naver.com>
2023-08-15 02:43:51 +0900

    [ADD] main > project > mobile

--- src/components/units/main/section03/mobile/Mobile.container.tsx
+++ src/components/units/main/section03/mobile/Mobile.container.tsx
@@ -12,15 +12,12 @@ import { Fade } from "react-awesome-reveal";
 export default function TabMobile() {
   const [isHover01, setIsHover01] = useState(false);
   const [isHover02, setIsHover02] = useState(false);
-  const [isHover03, setIsHover03] = useState(false);
 
   const handleMouse = (num: number) => () => {
     if (num === 1) {
       setIsHover01((prev) => !prev);
     } else if (num === 2) {
       setIsHover02((prev) => !prev);
-    } else if (num === 3) {
-      setIsHover03((prev) => !prev);
     }
   };
 
@@ -29,34 +26,28 @@ export default function TabMobile() {
       <LeftText>
         <Content triggerOnce={true}>
           <span>
+            모바일페이지 사이드 프로젝트는 모바일 기기에서도 뛰어난 접근성과
+            사용의 용이성을 유지하며 웹앱사이트를 구현했습니다.
+          </span>
+        </Content>
+        <Content triggerOnce={true}>
+          <span>
+            반응형 디자인과&nbsp;
             <em onMouseEnter={handleMouse(1)} onMouseLeave={handleMouse(1)}>
-              새로운 아이디어를 코드로 구현
+              모바일 터치 인터랙션
             </em>
-            하는 과정을 즐기며, 사용자 경험을 개선하고 멋진 인터페이스를 구축
-            하는 일에 큰 흥미를 가지고 있습니다. 끊임없이 발전하는 기술 동향을
-            따라가며 배우는 것을 좋아하며,&nbsp;
+            을 고려하여 사용자가 편안하게 사이트를 이용하는것을 최우선으로
+            생각하여 프로젝트를 진행하였습니다.
+          </span>
+        </Content>
+        <Content triggerOnce={true}>
+          <span>
+            현재는 웹앱으로 퍼블리싱되었지만, 더 나아가 실제 앱 개발에
+            도전하고자 합니다. 앱 개발을 위한 언어와 기술을 배워&nbsp;
             <em onMouseEnter={handleMouse(2)} onMouseLeave={handleMouse(2)}>
-              코드를 작성할때 가독성과 유지보수
+              실제 애플리케이션을 만들어보는 것
             </em>
-            를 고려한 코드를 작성하는것에 집중합니다.
-          </span>
-        </Content>
-        <Content triggerOnce={true}>
-          <span>
-            포트폴리오에는 저의 프론트엔드&nbsp;
-            <em onMouseEnter={handleMouse(3)} onMouseLeave={handleMouse(3)}>
-              개발자로서의 성장과정
-            </em>
-            을 확인하실 수 있도록 다양한 사이드 프로젝트들을 담았습니다.
-            최근에는 문제 해결 과정을 Readme를 통해 확인 할 수 있도록 정리하며
-            진행하고있습니다.
-          </span>
-        </Content>
-        <Content triggerOnce={true}>
-          <span>
-            포트폴리오를 통해 저에 대해 더 자세히 알아가시면 좋을 것 같습니다.
-            <br />
-            감사합니다:) ☺️
+            이 제 도전과제입니다.
           </span>
         </Content>
       </LeftText>
@@ -66,15 +57,11 @@ export default function TabMobile() {
           <Image>
             {isHover01 ? (
               <Fade>
-                <img src="/images/intro1.gif" alt="intro" />
+                <img src="/images/mobile1.gif" alt="mobile" />
               </Fade>
             ) : isHover02 ? (
               <Fade>
-                <img src="/images/intro2.gif" alt="intro" />
-              </Fade>
-            ) : isHover03 ? (
-              <Fade>
-                <img src="/images/intro3.gif" alt="intro" />
+                <img src="/images/mobile2.gif" alt="mobile" />
               </Fade>
             ) : (
               ""
