fa8afd7f401b9b806b849274f2901264c9b78de4
larmong <larmong@naver.com>
2023-08-24 15:02:06 +0900

    [ADD] profile page

--- src/components/units/profile/Profile.container.tsx
+++ src/components/units/profile/Profile.container.tsx
@@ -1,120 +1,173 @@
 import Progress01 from "../../commons/progresses/progress01/Progress01.container";
-import { Contents, Wrapper } from "./Profile.style";
+import {
+  Contents,
+  Hand,
+  List,
+  Main,
+  ProfileCont,
+  ProfileWrapper,
+  Stacks,
+  StacksContainer,
+  StacksWrapper,
+  Title,
+  Wrapper,
+} from "./Profile.style";
+import { BsCheck2Square, BsCodeSlash } from "react-icons/bs";
+import { MdHistory, MdSchool } from "react-icons/md";
+import { PiCertificateBold } from "react-icons/pi";
 
 export default function Profile() {
   return (
     <Wrapper>
       <Progress01 />
       <Contents>
-        <div>
-          <h5>끊임없는 도전을 하는 프론트엔드 개발자 "이아름"입니다! 👩🏻‍💻</h5>
-          <ul>
+        <Main>
+          <Title>
+            <Hand>
+              <div className="wave" role="img" aria-label="Waving hand">
+                👋
+              </div>
+            </Hand>
+            끊임없는 도전을 하는<span>FRONT-END</span>"이아름" 입니다!
+          </Title>
+          <List>
             <li>
+              <BsCheck2Square />
               꾸준함의 진짜 가치를 알기에 조그마한 프로젝트라도 도전하고
               실행합니다.
             </li>
             <li>
+              <BsCheck2Square />
               당장은 풀지 못하는 문제가 생기더라도 포기하지 않고 결국 해내는것을
               즐거워합니다.
             </li>
-            <li>새로운 개발 지식을 배우는 것을 좋아합니다.</li>
-          </ul>
-        </div>
-        <div>
-          <h5>education</h5>
-          <ul>
             <li>
-              <span>2023.03 ~ ing</span>방송통신대학교 컴퓨터과학과 편입
+              <BsCheck2Square />
+              새로운 개발 지식을 배우는 것을 좋아합니다.
             </li>
-            <li>
-              <span>2023.02 ~ 2023.05</span>코드캠프 고농축 프론트엔드 코스
-              [인프런X코드캠프]
-            </li>
-            <li>
-              <span>2020.06 ~ 2020.11</span>웹표준 기반 UIUX 엔지니어링
-              [더조은아카데미]
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h5>history</h5>
-          <ul>
-            <li>
-              <span>2022.04 ~ 2022.12</span>
-              <em>찌읏</em>프론트엔드
-            </li>
-            <li>
-              <span>2023.02 ~ 2023.05</span>
-              <em>히포케이메논</em>웹퍼블리셔 및 웹디자이너
-            </li>
-            <li>
-              <span>2018.06 ~ 2020.05</span>
-              <em>동아피엠에스</em>웹디자이너 및 사무
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h5>certificate</h5>
-          <ul>
-            <li>
-              <span>2020.12.10</span>
-              웹디자인 기능사
-            </li>
-            <li>
-              <span>2020.10.15</span>GTQ 1급
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h5>stacks</h5>
-          <span>
-            간단한 스택 목록입니다. 자세한 skill tree는 SKILL 페이지에서
-            확인해주세요!
-          </span>
-          <div>
-            <h6>front-End</h6>
+          </List>
+        </Main>
+        <ProfileWrapper>
+          <ProfileCont>
+            <h5>
+              <MdHistory />
+              history
+            </h5>
             <ul>
-              <li>HTML, CSS, Styled-Components(Emotion)</li>
-              <li>JavaScript, TypeScript</li>
-              <li>ReactJs, NextJs</li>
-              <li>Vue.js</li>
+              <li>
+                <span>2022.04 ~ 2022.12</span>
+                <em>찌읏</em>프론트엔드
+              </li>
+              <li>
+                <span>2022.02 ~ 2022.05</span>
+                <em>히포케이메논</em>웹퍼블리셔 및 웹디자이너
+              </li>
+              <li>
+                <span>2018.06 ~ 2020.05</span>
+                <em>동아피엠에스</em>웹디자이너 및 사무
+              </li>
             </ul>
-          </div>
-          <div>
-            <h6>back-End</h6>
+          </ProfileCont>
+          <ProfileCont>
+            <h5>
+              <MdSchool />
+              education
+            </h5>
             <ul>
-              <li>Node.js</li>
-              <li>Firebase, MonggoDB</li>
-              <li>GraphQL</li>
+              <li>
+                <span>2023.03 ~ ing</span>방송통신대학교 컴퓨터과학과 편입
+              </li>
+              <li>
+                <span>2023.02 ~ 2023.05</span>코드캠프 고농축 프론트엔드 코스
+                [인프런X코드캠프]
+              </li>
+              <li>
+                <span>2020.06 ~ 2020.11</span>웹표준 기반 UIUX 엔지니어링
+                [더조은아카데미]
+              </li>
             </ul>
-          </div>
-          <div>
-            <h6>배포</h6>
+          </ProfileCont>
+          <ProfileCont>
+            <h5>
+              <PiCertificateBold />
+              certificate
+            </h5>
             <ul>
-              <li>github pages</li>
-              <li>Netlify</li>
-              <li>Heroku</li>
-              <li>AWS</li>
+              <li>
+                <span>2020.12.10</span>
+                웹디자인 기능사
+              </li>
+              <li>
+                <span>2020.10.15</span>GTQ 1급
+              </li>
             </ul>
-          </div>
-          <div>
-            <h6>design</h6>
-            <ul>
-              <li>Photoshop, Illustrator</li>
-              <li>Figma, Adobe XD</li>
-            </ul>
-          </div>
-          <div>
-            <h6>collaboration</h6>
-            <ul>
-              <li>Github, Bitbuket</li>
-              <li>Sourcetree, Jjira</li>
-              <li>Slack, Notion</li>
-              <li>Postman, Playground</li>
-              <li>Web Storm, VS Code</li>
-            </ul>
-          </div>
-        </div>
+          </ProfileCont>
+        </ProfileWrapper>
+        <StacksWrapper>
+          <h5>
+            <BsCodeSlash />
+            stacks
+          </h5>
+          <span>자세한 STACKS은 SKILL 페이지에서 확인해주세요!</span>
+          <StacksContainer>
+            <Stacks>
+              <h6>디자인</h6>
+              <ul>
+                <li>Photoshop</li>
+                <li>Illustrator</li>
+                <li>Figma</li>
+                <li>Adobe XD</li>
+              </ul>
+            </Stacks>
+            <Stacks>
+              <h6>프론트엔드</h6>
+              <ul>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>Styled-Components(Emotion)</li>
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>NextJs</li>
+                <li>ReactJs</li>
+                <li>VueJs</li>
+                <li>Flutter</li>
+              </ul>
+            </Stacks>
+            <Stacks>
+              <h6>백엔드</h6>
+              <ul>
+                <li>NodeJs</li>
+                <li>Firebase</li>
+                <li>MongoDB</li>
+                <li>GraphQL</li>
+              </ul>
+            </Stacks>
+            <Stacks>
+              <h6>배포</h6>
+              <ul>
+                <li>Github Pages</li>
+                <li>Netlify</li>
+                <li>Heroku</li>
+                <li>AWS</li>
+              </ul>
+            </Stacks>
+            <Stacks>
+              <h6>협업 & 툴</h6>
+              <ul>
+                <li>Github</li>
+                <li>Bitbuket</li>
+                <li>Jjira</li>
+                <li>Sourcetree</li>
+                <li>Slack</li>
+                <li>Notion</li>
+                <li>Postman</li>
+                <li>Playground</li>
+                <li>Web Storme</li>
+                <li>VS Code</li>
+              </ul>
+            </Stacks>
+          </StacksContainer>
+        </StacksWrapper>
       </Contents>
     </Wrapper>
   );
--- src/components/units/profile/Profile.style.ts
+++ src/components/units/profile/Profile.style.ts
@@ -1,5 +1,167 @@
 import styled from "@emotion/styled";
+import { Common } from "../../../commons/style/emotion";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  padding: 100px 0;
+`;
 
-export const Contents = styled.div``;
+export const Contents = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Main = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+  margin-bottom: 200px;
+`;
+export const Hand = styled.div`
+  margin-left: -80px;
+  margin-bottom: 30px;
+  .wave {
+    font-size: 8rem;
+    display: inline-block;
+    animation: wave 1000ms infinite alternate ease-in-out;
+    transform-origin: 75% 80%;
+  }
+
+  @keyframes wave {
+    from {
+      transform: rotate(-10deg);
+    }
+    to {
+      transform: rotate(30deg);
+    }
+  }
+`;
+export const Title = styled.div`
+  font-size: 2.2em;
+  text-align: center;
+  font-weight: bold;
+  span {
+    display: block;
+    font-size: 3em;
+    background-image: linear-gradient(160deg, #ffcc60 0%, #ff5c00 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+`;
+export const List = styled.ul`
+  font-family: ${Common.font.text};
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  font-size: 1.4em;
+  margin: 0 auto;
+  li {
+    display: flex;
+    align-items: center;
+    svg {
+      margin-right: 20px;
+    }
+  }
+`;
+
+export const ProfileWrapper = styled.section`
+  display: flex;
+  border-top: ${Common.line.style};
+  padding: 100px 0;
+`;
+export const ProfileCont = styled.div`
+  text-align: center;
+  font-size: 1.2em;
+  width: calc(33.33% - 15px);
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  h5 {
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    font-size: 2.6em;
+    text-transform: uppercase;
+    color: #ffaf59;
+    background-image: linear-gradient(160deg, #ffcc60 0%, #ff5c00 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+    li {
+      font-family: ${Common.font.text};
+      span {
+        display: block;
+        font-weight: bold;
+        font-size: 1.2em;
+        font-family: ${Common.font.default};
+      }
+      em {
+        font-family: ${Common.font.text};
+        display: inline-block;
+        margin-right: 5px;
+        font-weight: bold;
+      }
+    }
+  }
+`;
+
+export const StacksWrapper = styled.section`
+  border-top: ${Common.line.style};
+  border-bottom: ${Common.line.style};
+  padding: 100px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  h5 {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    font-size: 3em;
+    text-transform: uppercase;
+    color: #ffaf59;
+    background-image: linear-gradient(160deg, #ffcc60 0%, #ff5c00 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  span {
+    opacity: 0.8;
+    display: inline-block;
+    font-size: 1em;
+    font-family: ${Common.font.text};
+    margin-bottom: 100px;
+  }
+`;
+export const StacksContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const Stacks = styled.div`
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  h6 {
+    display: block;
+    font-weight: bold;
+    font-size: 2em;
+  }
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+    li {
+      font-family: ${Common.font.text};
+      display: inline-block;
+      margin-right: 5px;
+      font-weight: bold;
+    }
+  }
+`;
